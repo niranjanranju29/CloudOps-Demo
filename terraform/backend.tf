56d085f00cf10658feeaf07bbaa88e251d53be83
niranjanar1 <nirnajana.r@philips.com>
2025-09-20 18:59:15 +0530

    updating-backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "cloudops-demo-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "cloudops-demo-locks"  # for state locking (optional)
     encrypt        = true
+    use_lockfile   = true
   }
 }
