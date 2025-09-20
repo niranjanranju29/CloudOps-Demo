6af93d257f7bb56143e69106bfd2b150de76d209
niranjanar1 <nirnajana.r@philips.com>
2025-09-20 18:28:42 +0530

    updatedtfprovider

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -12,7 +12,8 @@ variable "docker_image" {
 }
 
 
-variable "aws_profile" {
-  description = "AWS CLI profile to use"
-  default     = "ramesh"
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
